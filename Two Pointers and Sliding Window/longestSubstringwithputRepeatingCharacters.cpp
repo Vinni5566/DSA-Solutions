508c0a4118bfa11c0143d0ad1e08bc6e22783556
Vinni Kapoor <vinni3871@gmail.com>
2025-11-18 03:05:40 +0530

    Add new question

---
+++ Two Pointers and Sliding Window/longestSubstringwithputRepeatingCharacters.cpp
@@ -0,0 +1,39 @@
+//Problem link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        int l = s.length();
+
+        if(l == 0 || l == 1) return l;
+
+        int i = 0;
+        int j = 0;
+
+        unordered_set<char> set;
+        int maxLen = 0;
+
+        while(j < l) {
+
+            char ch = s[j];
+
+            if(set.find(ch) != set.end()) {
+                maxLen = max(maxLen, static_cast<int>(set.size()));
+                set.erase(s[i]);
+                i++;
+            } else {
+                set.insert(ch);
+                j++;
+            }
+        }
+
+        maxLen = max(maxLen, static_cast<int>(set.size()));
+
+        return maxLen;
+    }
+};
